08f79754a8cdb1cf87564d6b5d80d5334c479cdb
Tilda Böröcz <borocz.tilda@gmail.com>
2022-01-12 14:09:44 +0100

    databaseSourcePage kész van

--- Desktop/src/Vizsgaremek/Views/Pages/DatabaseSourcePage.xaml.cs
+++ Desktop/src/Vizsgaremek/Views/Pages/DatabaseSourcePage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Vizsgaremek.Views.Navigation;
 
 namespace Vizsgaremek.Views.Pages
 {
@@ -23,6 +24,12 @@ namespace Vizsgaremek.Views.Pages
         public DatabaseSourcePage()
         {
             InitializeComponent();
+        }
+
+        private void Image_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            WelcomePage welcomePage = new WelcomePage();
+            Navigate.Navigation(welcomePage);
         }
     }
 }
